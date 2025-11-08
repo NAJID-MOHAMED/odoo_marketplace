e377a659b7a0ce863a3e71c881b94bf2730fc8af
NAJID-MOHAMED <najidmohamed2026@gmail.com>
2025-11-08 10:21:00 +0100

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo MarketPlace ",
+    'version': '1.0.0',
+    'category': 'Sales',
+    'summary': "Module base d'une marketplace multi-fournisseurs",
+    'author': "Ton Nom",
+    'website': "http://www.example.com",
+    'license': "AGPL-3",
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
